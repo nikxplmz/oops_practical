602464d1ee381400804e1571a8ae9efe8b8ef5b5
Nikhil Valluri <nikhilvalluri123@gmail.com>
2025-08-04 08:48:56 +0530

    Add files via upload

---
+++ searchIndex.java
@@ -0,0 +1,10 @@
+package oops_practical;
+
+public class searchIndex {
+	 public static void main(String[] args) {
+		 String s = "Hello_world";
+	        System.out.println(s.indexOf("_"));        
+	        System.out.println(s.lastIndexOf("or"));    
+	        System.out.println(s.contains("ll"));      
+}
+ }
